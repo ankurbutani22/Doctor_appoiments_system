37a399684edaee9652d0ad55905f3a436945b12b
Your Name <your_email@example.com>
2026-02-25 17:43:12 +0530

    Fix: Correct Header component import path

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import Topdoctors from '../components/Topdoctors'
 import Banner from '../components/Banner'
