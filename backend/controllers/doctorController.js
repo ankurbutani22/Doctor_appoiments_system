baadf4273b4bd1aa5bc4d97796a9102eee53c583
Your Name <your_email@example.com>
2026-02-25 17:22:39 +0530

    move vite to dependencies and fix build

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,4 @@
-import doctorModel from "../models/doctormodel.js"
+import doctorModel from "../models/doctorModel.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import appointmentModel from "../models/appointmentModel.js"
