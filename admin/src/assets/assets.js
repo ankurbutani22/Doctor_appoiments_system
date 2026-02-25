99c0f001d452dc4edaaa4d999d5d2b875d63f528
Your Name <your_email@example.com>
2026-02-25 20:22:18 +0530

    move vite to dependencies and fix build

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -11,6 +11,7 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
+import patient_icon from './patient_icon.svg'
 
 export const assets = {
     add_icon,
@@ -21,6 +22,7 @@ export const assets = {
     upload_area,
     home_icon,
     patients_icon,
+    patient_icon,
     people_icon,
     list_icon,
     tick_icon,
